4054dcb50816f5554684bd2a00223b4945a10310
Nils0009 <143409567+Nils0009@users.noreply.github.com>
2024-05-30 15:32:11 +0200

    Course page with categories and searchbar done. CourseDetails under construction

---
+++ BlazorWebApp-Silicon.Bussiness/Models/CourseCardModel.cs
@@ -0,0 +1,22 @@
+﻿namespace BlazorWebApp_Silicon.Bussiness.Models;
+
+public class CourseCardModel
+{
+    public string? Id { get; set; }
+    public string? ImageUri { get; set; }
+    public string? ImageHeaderUri { get; set; }
+    public bool IsBestSeller { get; set; }
+    public bool IsDigital { get; set; }
+    public string? Author { get; set; }
+    public string? Price { get; set; }
+    public string? DiscountPrice { get; set; }
+    public string[]? Categories { get; set; }
+    public string? Title { get; set; }
+    public string? Ingress { get; set; }
+    public decimal StarRating { get; set; }
+    public string? Reviews { get; set; }
+    public string? LikesInProcent { get; set; }
+    public string? Likes { get; set; }
+    public string? Hours { get; set; }
+    public bool IsVisible { get; set; } = true;
+}
---
+++ BlazorWebApp-Silicon.Bussiness/Models/GraphQLQuery.cs
@@ -0,0 +1,6 @@
+﻿namespace BlazorWebApp_Silicon.Bussiness.Models;
+
+public class GraphQLQuery
+{
+    public string Query { get; set; } = null!;
+}
